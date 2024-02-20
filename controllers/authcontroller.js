a281bcb2f7470581e906ea3be5a80957f6095030
Dinanath <dkmdkm26@gmail.com>
2024-02-20 22:00:52 +0545

    updated

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -48,7 +48,7 @@ const Loginrouter = async (req, res) => {
       if (isMatch) {
         res.status(200).send("Login successfull");
       } else {
-        res.send({ messege: "ivalid password" });
+        res.send({ messege: "ivalid credtional" });
       }
     }
   } catch (error) {
