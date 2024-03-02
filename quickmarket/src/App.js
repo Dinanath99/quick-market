75f8df36b6445d497d621a9631a345ed1c74a918
Dinanath <dkmdkm26@gmail.com>
2024-03-02 08:58:11 +0545

    React functionality added

--- quickmarket/src/App.js
+++ quickmarket/src/App.js
@@ -1,12 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
+import PageNotFound from "./pages/PageNotFound";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
   );
