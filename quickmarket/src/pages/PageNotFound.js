c0e77bdc806b90bdbb0aee5a981e85070c53d32c
Dinanath <dkmdkm26@gmail.com>
2024-03-03 15:16:33 +0545

    Ui updated

--- quickmarket/src/pages/PageNotFound.js
+++ quickmarket/src/pages/PageNotFound.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
   return (
     <Layout>
-      <h1>Page not found</h1>
+      <div className="pnf">
+        <h1 className="pnf-title">404</h1>
+        <h2 className="pnf-heading">Oops ! Page Not found</h2>
+
+        <Link to="/" className="png-btn">
+          Go back
+        </Link>
+      </div>
     </Layout>
   );
 };
