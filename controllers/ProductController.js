ca57fee558bab34836b8c9004e1200156fe0587c
Dinanath <dkmdkm26@gmail.com>
2024-03-07 08:43:24 +0545

    Create product added

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,3 +1,47 @@
-const createProductController = () => {};
+const ProductModel = require("../models/ProductModel.js");
+const fs = require("fs");
+const slugify = require("slugify");
+const createProductController = async (req, res) => {
+  try {
+    const { name, description, price, category, quantity, shipping } =
+      req.fields;
+    const { photo } = req.files;
+    //alidation
+    switch (true) {
+      case !name:
+        return res.status(500).send({ error: "Name is Required" });
+      case !description:
+        return res.status(500).send({ error: "Description is Required" });
+      case !price:
+        return res.status(500).send({ error: "Price is Required" });
+      case !category:
+        return res.status(500).send({ error: "Category is Required" });
+      case !quantity:
+        return res.status(500).send({ error: "Quantity is Required" });
+      case photo && photo.size > 1000000:
+        return res
+          .status(500)
+          .send({ error: "photo is Required and should be less then 1mb" });
+    }
 
-module.exports = createProductController;
+    const products = new ProductModel({ ...req.fields, slug: slugify(name) });
+    if (photo) {
+      products.photo.data = fs.readFileSync(photo.path);
+      products.photo.contentType = photo.type;
+    }
+    await products.save();
+    res.status(201).send({
+      success: true,
+      message: "Product Created Successfully",
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error in crearing product",
+    });
+  }
+};
+module.exports = { createProductController };
