1fd852b83f334d8d8ed140e3c63d5df7b63cccc0
Dinanath <dkmdkm26@gmail.com>
2024-03-07 22:38:57 +0545

    category added

--- quickmarket/src/pages/Admin/CreateCategory.js
+++ quickmarket/src/pages/Admin/CreateCategory.js
@@ -3,9 +3,31 @@ import AdminMenu from "../../components/Layout/AdminMenu";
 import Layout from "../../components/Layout";
 import toast from "react-hot-toast";
 import axios from "axios";
-
+import CategoryForm from "../../components/Form/CategoryForm";
+import { Modal } from "antd";
 const CreateCategory = () => {
   const [categories, setCategories] = useState([]);
+  const [name, setName] = useState("");
+  const [visible, setVisible] = useState(false);
+
+  //handle formf
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("/api/category/create-category", {
+        name,
+      });
+      if (data?.success) {
+        toast.success(`${name} is created`);
+        getAllCategory();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong in creating category");
+    }
+  };
 
   //get all categories
   const getAllCategory = async () => {
@@ -32,6 +54,13 @@ const CreateCategory = () => {
           </div>
           <div className="col-md-9">
             <h1>Manage Category</h1>
+            <div className="p-3 w-50">
+              <CategoryForm
+                handleSubmit={handleSubmit}
+                value={name}
+                setValue={setName}
+              />
+            </div>
             <div className="w-75">
               <table className="table">
                 <thead>
@@ -45,13 +74,24 @@ const CreateCategory = () => {
                     <tr key={c._id}>
                       <td>{c.name}</td>
                       <td>
-                        <button className="btn btn-primary">Edit</button>
+                        <button
+                          className="btn btn-primary ms-2"
+                          onClick={() => setVisible(true)}
+                        >
+                          Edit
+                        </button>
+                        <button className="btn btn-danger ms-2">Delete</button>
                       </td>
                     </tr>
                   ))}
                 </tbody>
               </table>
             </div>
+            <Modal
+              onCancel={() => setVisible(false)}
+              footer={null}
+              visible={visible}
+            ></Modal>
           </div>
         </div>
       </div>
